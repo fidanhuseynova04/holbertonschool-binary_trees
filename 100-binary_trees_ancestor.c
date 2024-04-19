071aea4c10098ef42a7a710e76768d83d4f964f3
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-04-19 16:09:37 +0000

    100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -7,17 +7,24 @@
  *
  * Return: node or NULL
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
 {
 	if (!first || !second)
 	{
 		return (NULL);
 	}
-        if (first == second)
-                return ((binary_tree_t *)first);
-        if (binary_tree_depth(first) > binary_tree_depth(second))
-                return (binary_trees_ancestor(first->parent, second));
-        if (binary_tree_depth(second) > binary_tree_depth(first))
-                return (binary_trees_ancestor(first, second->parent));
-        return (binary_trees_ancestor(first->parent, second->parent));
+	if (first == second)
+	{
+		return ((binary_tree_t *)first);
+	}
+	if (binary_tree_depth(first) > binary_tree_depth(second))
+	{
+		return (binary_trees_ancestor(first->parent, second));
+	}
+	if (binary_tree_depth(second) > binary_tree_depth(first))
+	{
+		return (binary_trees_ancestor(first, second->parent));
+	}
+	return (binary_trees_ancestor(first->parent, second->parent));
 }
