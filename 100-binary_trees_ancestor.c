2ccda9305ce264bf845073231a9bdc1362916abb
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-04-19 16:00:34 +0000

    100-binary_trees_ancestor.c

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - finds lowest ancestor of two nodes
+ * @first: first node
+ * @second: second node
+ *
+ * Return: node or NULL
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+                const binary_tree_t *second)
+{
+        if (!first || !second)
+                return (NULL);
+        if (first == second)
+                return ((binary_tree_t *)first);
+        if (binary_tree_depth(first) > binary_tree_depth(second))
+                return (binary_trees_ancestor(first->parent, second));
+        if (binary_tree_depth(second) > binary_tree_depth(first))
+                return (binary_trees_ancestor(first, second->parent));
+        return (binary_trees_ancestor(first->parent, second->parent));
+}
